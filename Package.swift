3f6313b86c8407c679a86557f3781a41d0ce2882
Jacob Wlliams <ponyboy47@gmail.com>
2016-11-14 17:30:29 -0700

    argument 'testDependencies' must precede argument 'dependencies'

--- Package.swift
+++ Package.swift
@@ -2,10 +2,10 @@ import PackageDescription
 
 let package = Package(
   name: "Stencil",
-  dependencies: [
-    .Package(url: "https://github.com/necolt/PathKit.git", majorVersion: 0, minor: 6),
-  ],
   testDependencies: [
     .Package(url: "https://github.com/kylef/spectre-build", majorVersion: 0),
+  ],
+  dependencies: [
+    .Package(url: "https://github.com/necolt/PathKit.git", majorVersion: 0, minor: 6),
   ]
 )
